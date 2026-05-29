c5957a517dd793758b74cce0a82993ad9b045921
Utkarsh Tiwari <64308492+norsparyt@users.noreply.github.com>
2026-05-30 00:06:38 +0530

    Create 63-UniquePathsIi

---
+++ 63-UniquePathsIi/63-UniquePathsIi.java
@@ -0,0 +1,26 @@
+// Last updated: 30/05/2026, 00:06:38
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length, col = obstacleGrid[0].length;
+        int dp[][] = new int[row][col];
+        for (int i = 0; i < row; i++)
+            Arrays.fill(dp[i], -1);
+        //if (row == 1 && col == 1){ reduntant check = if start or destination is blocked - no path anyways
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[row - 1][col - 1] == 1)
+            return 0;
+        return findPaths(0, 0, row, col, obstacleGrid, dp);
+    }
+
+    int findPaths(int r, int c, int row, int col, int[][] obstacleGrid, int[][] dp) {
+        if (r >= row || c >= col)
+            return 0;
+        if (obstacleGrid[r][c] == 1)
+            return 0;
+        if (r == row - 1 && c == col - 1)
+            return 1;
+        if (dp[r][c] != -1)
+            return dp[r][c];
+        return dp[r][c] = findPaths(r + 1, c, row, col, obstacleGrid, dp)
+                + findPaths(r, c + 1, row, col, obstacleGrid, dp);
+    }
+}
