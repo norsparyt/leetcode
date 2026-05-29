5240d3799cba3d676f0b606e254e99326723ffd7
Utkarsh Tiwari <64308492+norsparyt@users.noreply.github.com>
2026-05-30 00:05:01 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -0,0 +1,23 @@
+// Last updated: 30/05/2026, 00:05:01
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], -1);
+        }
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty())
+                map.put(nums2[i], stack.peek());
+            stack.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.length; i++)
+            nums1[i] = map.get(nums1[i]);
+        return nums1;
+    }
+}
