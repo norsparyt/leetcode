974a53f46488b75fe32bd1e367c1ae3176579e5a
Utkarsh Tiwari <64308492+norsparyt@users.noreply.github.com>
2026-05-30 00:05:35 +0530

    Create 208-ImplementTriePrefixTree

---
+++ 208-ImplementTriePrefixTree/208-ImplementTriePrefixTree.java
@@ -0,0 +1,58 @@
+// Last updated: 30/05/2026, 00:05:35
+class TrieNode{
+    TrieNode[] children;
+    boolean isEnd;
+    TrieNode(){
+        this.children = new TrieNode[26];
+        this.isEnd=false;
+    }
+}
+class Trie {
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if(curr.children[index] == null) 
+                curr.children[index] = new TrieNode();
+            curr = curr.children[index];
+        }
+        curr.isEnd = true;
+
+    }
+    
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if(curr.children[index] == null) 
+                return false;
+            curr = curr.children[index];
+        }
+        return curr.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if(curr.children[index] == null) 
+                return false;
+            curr = curr.children[index];
+        }
+        return true;
+    }
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
