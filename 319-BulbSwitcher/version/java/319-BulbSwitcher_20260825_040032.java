cd3c1ce6cc3f4131a791a67e93c7e68411b151c7
Utkarsh Tiwari <64308492+norsparyt@users.noreply.github.com>
2026-08-25 04:00:33 +0530

    Create 319-BulbSwitcher

---
+++ 319-BulbSwitcher/version/java/319-BulbSwitcher_20260825_040032.java
@@ -0,0 +1,14 @@
+// Last updated: 25/08/2026, 04:00:32
+// Maths.
+1class Solution {
+2    public int bulbSwitch(int n) {
+3        //no of squares between 1 and n = sqrt(n)
+4        return (int) Math.sqrt(n);
+5        // int count = 0;
+6        // for(int i = 1; i <= n; i++){
+7        //     if(i <= n/i)
+8        //         count++;
+9        // }
+10        // return count;
+11    }
+12}
