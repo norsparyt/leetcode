8d7c721794e7d0833c256f477d9dc93afdf078e2
Utkarsh Tiwari <64308492+norsparyt@users.noreply.github.com>
2026-09-10 02:50:39 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/version/java/645-SetMismatch_20260910_025038.java
@@ -0,0 +1,17 @@
+// Last updated: 10/09/2026, 02:50:38
+// O(n) space
+1class Solution {
+2    public int[] findErrorNums(int[] nums) {
+3        int n = nums.length;
+4        int[] arr = new int[n];
+5        int[] ans = new int[2];
+6        for(int i=0;i <n;i++){
+7            arr[nums[i]-1]++;
+8        }
+9        for(int i=0;i <n;i++){
+10            if(arr[i] == 2) ans[0] = i+1;
+11            else if(arr[i] == 0) ans[1] = i+1;
+12        }
+13        return ans;
+14    }
+15}
